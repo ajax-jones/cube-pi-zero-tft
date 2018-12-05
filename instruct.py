2029e87d39016b5ae81530ced9833ee874399bf2
ajax-jones <ajaxjones@icloud.com>
2018-12-05 11:16:45 +0000

    Update instruct.py

--- instruct.py
+++ instruct.py
@@ -1,15 +1,18 @@
-#from bs4 import BeautifulSoup    
+#!/usr/bin/python
+########################################################################
+#
+# A Python script for retrieving views of projects of an Instructable Author
+# Author : Ajax Jones
+# Date : 5 Dec 2018
+# For more information, https://www.instructables.com/member/Ajaxjones/
+########################################################################    
 import urllib2 
 import re
 import requests
 import time
 
-#url = urllib2.urlopen("https://www.instructables.com/member/Ajaxjones/")    
-#content = url.read()    
-
-#print soup
-
-def get_url(url, time_out=600):
+# URL get routine with timeout options
+def get_url(url, time_out=120):
     r = requests.get(url)
     sleep_time = 1.0
     while r.status_code != 200:
@@ -19,28 +22,21 @@ def get_url(url, time_out=600):
         r = requests.get(url)
         sleep_time += 2
         if (sleep_time > time_out):
-            # We have sleeped for over 10 minutes, the page probably does
-            # not exist.
+            # We have slept for over 2 minutes, so we give up
             break
     if r.status_code != 200:
         print("failed to retrieve {}".format(url))
     else:
         return r
-        # return r.text
-
 
 def get_views(text):
-    myviews =[]
-#    pattern = "(\d+\s)Views"
-#    pattern = "total-views\">(\d+.\d{3})"
     pattern = "total-views\">((\d+).(\d{3}))"
     matched = re.findall(pattern, text)
-#    print ("m0",matched[0][2])
-    kio =  matched[0][1]+matched[0][2]
-#    print (kio)
-    return int(kio)
-#    return [int(val) for val in kio]
+    author_views =  matched[0][1]+matched[0][2]
+    return int(author_views)
 
+########################################################################    
+# Main Routine
 
 author = "Ajaxjones"
 base_url = "https://www.instructables.com/member/{}"
@@ -48,5 +44,4 @@ url = base_url.format(author)
 
 contents = get_url(url).text
 views  =  get_views(contents)
-#print (contents)
 print (views)
